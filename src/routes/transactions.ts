8ed4f5de295a9c86e4871fee60c2cdb651305975
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-28 18:55:14 -0300

    feat: created `POST` route for a new transaction

---
+++ src/routes/transactions.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { randomUUID } from 'node:crypto';
+import { knex } from '../database';
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import fastify from 'fastify';
-
-import { knex } from './database';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*');
-
-  return transactions;
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 });
 
 app.listen({ port: env.PORT }).then(() => {
