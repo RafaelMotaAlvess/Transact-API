ef07ad1d85a601cd41b8137d034f48cab479f2e7
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-28 19:09:57 -0300

    feat: created database interface for Knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex';
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string;
+      title: string;
+      amount: number;
+      created_at: string;
+      session_id?: string;
+    };
+  }
+}
