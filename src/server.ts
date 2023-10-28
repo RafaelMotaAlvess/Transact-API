af7c27836208a3b2ae61426120c4f91aaf3f313f
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-28 16:16:28 -0300

    feat: adding initial configuration files

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify';
+import { knex } from './database';
+
+const app = fastify();
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*');
+
+  return tables;
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!');
+});
