5585cd6f6ce436568b5303f6eb02455efcb1f1be
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-28 18:19:51 -0300

    feat: added environment variables

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
 import { knex as setupKnex, Knex } from 'knex';
+import { env } from './env';
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './database/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import fastify from 'fastify';
+
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*');
 
-  return tables;
+  return transactions;
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!');
 });
