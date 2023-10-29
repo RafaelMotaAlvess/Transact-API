9c7da017f44a5223d70a2f574a8536f858afba94
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-28 21:53:48 -0300

    feat: added validation for `session_id`

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,32 +2,51 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import { knex } from '../database';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select();
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
 
-    return {
-      transactions,
-    };
-  });
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select('*');
 
-  app.get('/:id', async request => {
+      return {
+        transactions,
+      };
+    }
+  );
+
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async request => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = getTransactionParamsSchema.parse(request.params);
 
-    const transaction = await knex('transactions').where('id', id).first();
+    const { sessionId } = request.cookies;
+
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first();
 
     return {
       transaction,
     };
   });
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async request => {
+    const { sessionId } = request.cookies;
+
     const summary = await knex('transactions')
+      .where('session_id', sessionId)
       .sum('amount', { as: 'amount' })
       .first();
 
@@ -45,7 +64,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     );
 
-    let sessionId = request.cookies.session_id;
+    let { sessionId } = request.cookies;
 
     if (!sessionId) {
       sessionId = randomUUID();
