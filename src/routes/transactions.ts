45551ef67b3c6062496da7c280492a70a8ede957
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-28 21:23:03 -0300

    feat: adding cookies for each user who creates a transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -45,10 +45,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     );
 
+    let sessionId = request.cookies.session_id;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     });
 
     return reply.status(201).send();
