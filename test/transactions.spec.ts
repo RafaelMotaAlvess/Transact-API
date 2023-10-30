b5fb3fe7a20c997477ccf485f5015748993ac3f5
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-30 12:56:13 -0300

    test: list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest';
+import { it, beforeAll, afterAll, describe, expect } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
 
@@ -20,5 +20,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new transaction',
+        amount: 5000,
+      }),
+    ]);
   });
 });
