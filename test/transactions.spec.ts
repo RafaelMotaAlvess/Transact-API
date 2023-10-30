090c5a0b1f6d10c13f105d775cb2f82b7606acce
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-30 13:38:45 -0300

    test: get the summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -82,4 +82,34 @@ describe('Transactions routes', () => {
       })
     );
   });
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      });
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    });
+  });
 });
