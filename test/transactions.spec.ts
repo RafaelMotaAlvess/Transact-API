24d14bd6283bb7b813bd4d593d63b1eab7621ed9
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-30 12:41:33 -0300

    test: create a new transaction

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { it, beforeAll, afterAll, describe } from 'vitest';
+import request from 'supertest';
+import { app } from '../src/app';
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201);
+  });
+});
