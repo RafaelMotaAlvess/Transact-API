4d052354549d5e5ea71dd19b2566fea1a4e69f79
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-10-30 16:16:24 -0300

    fix: deploy host error

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
   console.log('HTTP server running!');
 });
